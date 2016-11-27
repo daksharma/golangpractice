24aa31a1e986ece9b1ffab760ff091c56d8ef3c1
Daksh Sharma <dashatrix@gmail.com>
2016-11-26 21:43:00 -0800

    [MOD] - fib file dir change

--- hello.go
+++ hello.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+	fmt.Println(fib(2))
+}
+
+func fib(n int) int {
+	a := 0
+	b := 1
+	for count := 0; count < n; count++ {
+		a = b
+		b = a + b
+	}
+	return b
 }
